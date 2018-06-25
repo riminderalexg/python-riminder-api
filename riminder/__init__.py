e04e0260d382fd6c201f8216916d568306433244
Alexandre Gotte <alexandre.gotte@riminder.net>
2018-06-25 17:27:38 +0200

    Fixed imports

--- riminder/__init__.py
+++ riminder/__init__.py
@@ -1,1 +1,5 @@
-from .riminder import Riminder  # noqa
+from .riminder import Riminder
+from .profile import Profile
+from .filter import Filter
+from .source import Source
+from .webhook import Webhook  # noqa
